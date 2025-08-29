718dace1062496fe2575a60df3ad068f2dd51ceb
hgvutrannn <hoangvutrannn@gmail.com>
2025-08-29 09:55:30 -0400

    Fix sentiment service

--- sentiment/index.js
+++ sentiment/index.js
@@ -38,9 +38,9 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if (sentiment < 0) {sentiment = "negative";
+        if (analysisResult < 0) {sentiment = "negative";
         }
-        else {
+        else if (analysisResult > 0.33){
             sentiment = 'positive';
         }
 
@@ -48,7 +48,7 @@ app.post('/sentiment', async (req, res) => {
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
         // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment})
+        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment});
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
