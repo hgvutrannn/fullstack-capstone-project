ca210abffd2b8dc554e4673494c62926fa097987
Hoang Vu Tran <hgvutran@Hoangs-MacBook-Air.local>
2025-09-01 18:32:16 +0700

    Final app

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,7 +28,7 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
-
+const authRoutes = require('./routes/authRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
@@ -36,10 +36,11 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('api/gifts', giftRoutes);
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('api/search', searchRoutes);
+app.use('/api/search', searchRoutes);
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
